1abd81779b7bc36822ff84d8a693b7dbcb287206
Tomisin <tomisinjenrola@gmail.com>
2017-08-01 08:17:07 -0230

    Created color palette

---
+++ frontend/src/config/paletteConstants.js
@@ -0,0 +1,10 @@
+const palette = {
+	lightblue: "#2e9cca", 
+	white:     "#ffffff", 
+	deepblue:  "#25274d",
+	bluegrey:  "#464866", 
+	navyblue:  "#29648a", 
+	grey:      "#aaabb8"
+}
+
+export default palette
